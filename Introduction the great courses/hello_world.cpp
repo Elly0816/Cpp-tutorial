1474b4213c40bfbad86acdfeb157b188bfc84707
Elly0816 <elzoremmanuel@gmail.com>
2025-06-23 22:49:14 +0100

    initial commit

---
+++ Introduction the great courses/hello_world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv){
+
+    string name = "Eleazar";
+    cout << "Howdy "<< name << "!" << endl << "This is something else" << endl;
+
+    return 0;
+}
