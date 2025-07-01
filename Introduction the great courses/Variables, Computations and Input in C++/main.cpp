543774c704e6c310239f941faa097909e1a2bd5d
Elly0816 <elzoremmanuel@gmail.com>
2025-07-01 10:46:55 +0100

    finished with variables, computations, and input in C++

--- Introduction the great courses/Variables, Computations and Input in C++/main.cpp
+++ Introduction the great courses/Variables, Computations and Input in C++/main.cpp
@@ -5,13 +5,59 @@ This program computes calories using carbs, protein and fat
 */
 
 #include <iostream>
-#include <string>
 
 
 using namespace std;
 
 int main (){
 
+    cout<<"Enter your name: ";
+
+    string name;
+
+    cin>>name;
+
+    cout<<endl;
+
+    int age;
+
+    cout<<"Enter your age: ";
+
+    cin>>age;
+
+    cout<<endl;
+
+    cout<<"Your name is: "<<name<<" and your age is: "<<age<<endl;
+
+
+
+
+
+    // int a = 7, b=3;
+    // cout<<a/b<<endl;
+    // cout<<a%b<<endl; // in c++, modulus works only with integers, never foats or doubles.
+
+
+
+    // int weeks = 5, initial_days = 4, total_days = initial_days + (7*weeks);
+
+
+    // cout<<"There are "<<total_days<<" days in "<<weeks<< " weeks and "<<initial_days<<" days."<<endl;
+
+
+    // float carb_grams, protein_grams, fat_grams, calories;
+
+    // carb_grams = 30.45;
+    // protein_grams = 5.99;
+    // fat_grams = 15.65;
+
+
+    // calories = (4 * carb_grams) + (4*protein_grams)  + (9*fat_grams);
+
+    // cout<<"There are "<<calories<<" calories in the dish."<<endl;
 
     return 0;
 }
+
+
+
